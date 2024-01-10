4f906b71f23de8acbbf7900f312bc0511960089c
wookh <wook.heo89@gmail.com>
2024-01-10 23:22:19 +0900

    3th Commit  - JPA 추가1

--- src/main/java/com/group/libraryapp/config/UserConfiguration.java
+++ src/main/java/com/group/libraryapp/config/UserConfiguration.java
@@ -1,13 +1,11 @@
 package com.group.libraryapp.config;
 
-import com.group.libraryapp.repository.user.UserRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.group.libraryapp.repository.user.UserJdbcRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserConfiguration {
 
-    public UserRepository userRepository(JdbcTemplate jdbcTemplate) {
-        return new UserRepository(jdbcTemplate);
+    public UserJdbcRepository userRepository(JdbcTemplate jdbcTemplate) {
+        return new UserJdbcRepository(jdbcTemplate);
     }
 }
